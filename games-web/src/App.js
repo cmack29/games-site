e033447c7becdd72bf2faa5a83da9b8850e4841d
conor <conor.m@me.com>
2022-06-28 15:13:02 +0100

    snake

--- games-web/src/App.js
+++ games-web/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Routes, Route } from "react-router-dom"
 import Home from './pages/Home';
 import Tictactoe from './pages/Tictactoe';
+import Snake from './pages/Snake';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/tictactoe" element={<Tictactoe />} />
+      <Route path="/snake" element={<Snake />} />
     </Routes>
     </>
   );
--- games-web/src/pages/Home.jsx
+++ games-web/src/pages/Home.jsx
@@ -9,13 +9,17 @@ export default function Home() {
         navigate("/tictactoe")
     }
 
+    function snake() {
+        navigate("/snake")
+    }
+
     return(
         <>
         <h1>Hello</h1>
         <div className="home-styling">
             <ul className="grid-container">
             <li>{<button className="tictactoeButton home-button" onClick={ticTacToe}>Tic Tac Toe</button>}</li>
-            <li><button className="home-button">Lorum Ipsum</button></li>
+            <li>{<button className="home-button" onClick={snake}>Snake</button>}</li>
             <li><button className="home-button">Lorum Ipsum</button></li>
             <li><button className="home-button">Lorum Ipsum</button></li>
             </ul>
